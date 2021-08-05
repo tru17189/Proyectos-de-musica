28485d5589d9963d60937b81a02e3509d8187a26
tru17189 <32879914+tru17189@users.noreply.github.com>
2021-08-04 19:27:42 -0600

    Ajuste

--- Calculador de calidad de acordes/Calculador_de_calidad_acordes.py
+++ Calculador de calidad de acordes/Calculador_de_calidad_acordes.py
@@ -1,40 +1,41 @@
 teclado = ["Do", "Do#", "Re", "Re#", "Mi", "Fa", "Fa#", "Sol", "Sol#", "La", "La#", "Si",
            "Do", "Do#", "Re", "Re#", "Mi", "Fa", "Fa#", "Sol", "Sol#", "La", "La#", "Si",
-           "Do", "Do#", "Re", "Re#", "Mi", "Fa", "Fa#", "Sol", "Sol#", "La", "La#", "Si",
-           "do", "do#", "re", "re#", "mi", "fa", "fa#", "sol", "sol#", "la", "la#", "si",
-           "do", "do#", "re", "re#", "mi", "fa", "fa#", "sol", "sol#", "la", "la#", "si",
-           "do", "do#", "re", "re#", "mi", "fa", "fa#", "sol", "sol#", "la", "la#", "si"]
+           "Do", "Do#", "Re", "Re#", "Mi", "Fa", "Fa#", "Sol", "Sol#", "La", "La#", "Si"]
 teclado_nuevo = []
 posicion = 0
 posicion_2 = 0
 
 acorde_elegido = str(input("Escribe el acorde el cual quieres conocer su escala: "))
+respuesta_acorde = []
 
 for e in teclado:
     if acorde_elegido == e:
-        print("Original: "+teclado[posicion])
+        respuesta_acorde.append("Original: "+teclado[posicion])
         teclado_nuevo.append(teclado[posicion])
         
-        print("Tono: "+teclado[posicion+2])
+        respuesta_acorde.append("Tono: "+teclado[posicion+2])
         teclado_nuevo.append(teclado[posicion+2])
         
-        print("Tono: "+teclado[posicion+4])
+        respuesta_acorde.append("Tono: "+teclado[posicion+4])
         teclado_nuevo.append(teclado[posicion+4])
         
-        print("1/2 Tono: "+teclado[posicion+5])
+        respuesta_acorde.append("1/2 Tono: "+teclado[posicion+5])
         teclado_nuevo.append(teclado[posicion+5])
         
-        print("Tono: "+teclado[posicion+7])
+        respuesta_acorde.append("Tono: "+teclado[posicion+7])
         teclado_nuevo.append(teclado[posicion+7])
         
-        print("Tono: "+teclado[posicion+9])
+        respuesta_acorde.append("Tono: "+teclado[posicion+9])
         teclado_nuevo.append(teclado[posicion+9])
         
-        print("Tono: "+teclado[posicion+11])
+        respuesta_acorde.append("Tono: "+teclado[posicion+11])
         teclado_nuevo.append(teclado[posicion+11])
         
-        print("1/2 Tono: "+teclado[posicion+12])
+        respuesta_acorde.append("1/2 Tono: "+teclado[posicion+12])
         teclado_nuevo.append(teclado[posicion+12])
+
+        print("ESCALA DE %s:" %e)
+        print(respuesta_acorde)
 
         teclado_nuevo.append(teclado[posicion+2])
         teclado_nuevo.append(teclado[posicion+4])
@@ -51,13 +52,14 @@ for e in teclado:
         teclado_nuevo.append(teclado[posicion+11])
         teclado_nuevo.append(teclado[posicion+12])
 
-        print("\n\n-------Acordes-------")
+        respuesta_acorde = []
+        print("\n\nACORDES DE LA ESCALA DE %s:" %e)
         for i in range(0, 7):
-            print("---"+teclado_nuevo[posicion_2]+"---")
-            print("->"+teclado_nuevo[posicion_2])
-            print("->"+teclado_nuevo[posicion_2+2])
-            print("->"+teclado_nuevo[posicion_2+4])
-            print("\n")
+            respuesta_acorde.append("*** %s ***: -%s-%s-%s" %(teclado_nuevo[posicion_2], teclado_nuevo[posicion_2], 
+                                    teclado_nuevo[posicion_2+2], teclado_nuevo[posicion_2+4]))
             posicion_2 += 1
+
+        print("%s\n%s\n%s\n%s\n%s\n%s\n%s\n" %(respuesta_acorde[0], respuesta_acorde[1], respuesta_acorde[2],
+                respuesta_acorde[3], respuesta_acorde[4], respuesta_acorde[5], respuesta_acorde[6]))
         break
     posicion += 1
